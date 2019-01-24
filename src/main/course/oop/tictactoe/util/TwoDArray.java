30095d8056e7b4210b365f4b738ed1337a41b1c4
danielgreg2 <gdecanio@ufl.edu>
2019-01-23 22:55:32 -0500

    All functions are complete. insertInt() also prints out messages. Just returning strings does not print the messages.

--- src/main/course/oop/tictactoe/util/TwoDArray.java
+++ src/main/course/oop/tictactoe/util/TwoDArray.java
@@ -9,20 +9,28 @@ package main.course.oop.tictactoe.util;
  *
  */
 public class TwoDArray {
-	
+	//instance variables
+	int numRows;
+	int numCols;
+	int defaultVal;
+	int[][] twoDArr;
+	int[][] uniqueVal;
 	
 	public TwoDArray(int rows, int cols, int defaultVal){
 		/*TODO - Create a 2D integer array consisting of 
 		 * the number of rows and columns given. Initialize 
 		 * the array by setting each int to be the defaulVal. 
 		 * */
-		int[][] twoDArr = new int[rows][cols];	//creates a 2D array with #rows = rows, #cols = cols
+		this.numRows = rows;
+		this.numCols = cols;
+		this.defaultVal = defaultVal;
+		twoDArr = new int[rows][cols];	//creates a 2D array with #rows = rows, #cols = cols
 		//the following for loops will set each value of the 2D array to the defaultVal
 		for(int i = 0; i < rows; ++i)
 		{
 			for(int j = 0; j < cols; ++j)
 			{
-				towDArr[i][j] = defaultVal;
+				twoDArr[i][j] = defaultVal;
 			}
 		}
 	}
@@ -32,9 +40,9 @@ public class TwoDArray {
 		 * setting each int to be the defaulVal 
 		 */
 		//the following for loops will set each value of the 2D array to the defaultVal
-		for(int i = 0; i < rows; ++i)
+		for(int i = 0; i < numRows; ++i)
 		{
-			for(int j = 0; j < cols; ++j)
+			for(int j = 0; j < numCols; ++j)
 			{
 				twoDArr[i][j] = defaultVal;
 			}
@@ -55,7 +63,24 @@ public class TwoDArray {
 		 * Note: Print the int value in place of (). 
 		 * e.g., replace (val) with val.
 		 */
-		return "Not implemented";
+		StringBuffer returnMsg = new StringBuffer();
+		if(twoDArr[row][col] == defaultVal)
+		{
+			twoDArr[row][col] = val;
+			System.out.println("Success! " + val + " was inserted");
+			returnMsg.append("Success! ").append(val).append(" was inserted.");
+		}
+		else if(twoDArr[row][col] != defaultVal)
+		{
+			System.out.println("Failure: " + row + ", " + col + " is not empty");
+			returnMsg.append("Failure: ").append(row).append(", ").append(col).append(" is not empty.");
+		}
+		else
+		{
+			System.out.println("Failure: " + val + "is not allowed.");
+			returnMsg.append("Failure: ").append(val).append(" is not allowed.");
+		}
+		return returnMsg.toString();
 	}
 	
 	public int getInt(int row, int col) {
@@ -73,8 +98,18 @@ public class TwoDArray {
 		 *  0	1	1
 		 * 
 		 */
-		
-		return "Not implemented";
+		StringBuffer matrix = new StringBuffer();
+		String separator = " ";
+		for (int i = 0; i < numRows; ++i)
+		{
+		  for (int j = 0; j < numCols; ++j)
+		    if (j > 0)
+		      matrix.append(separator).append(twoDArr[i][j]);
+		    else
+		      matrix.append(separator).append("\n").append(twoDArr[i][j]);
+		}
+		return matrix.toString();
+		//return "Not implemented";
 	}
 	
 	public String getArrayDetails() {
@@ -89,50 +124,56 @@ public class TwoDArray {
 		 * 			)
 		 * 
 		 */
-		int maxUnique = twoDArr.length()*twoDArr[0].length();
-		System.out.println("# of rows: " + twoDArr.length());		//prints # of rows
-		System.out.println("# of columns: " + twoDrr[0].length());	//prints # of cols
-		int[maxUnique][2] uniqueVal = new int[maxUnique][2];	//2D array used to store unique values
-		for(int i = 0; i < twoDArr.length(); ++i)
+		StringBuffer details = new StringBuffer();
+		int maxUnique = numRows*numCols;
+		details.append("\n");
+		details.append("# of rows: ").append(numRows).append("\n");		//prints # of rows
+		details.append("# of columns: ").append(numCols);	//prints # of cols
+		uniqueVal = new int[maxUnique][2];	//2D array used to store unique values
+		for(int i = 0; i < numRows; ++i)
 		{
-			for(int j = 0; j < twoDArr[i].length(); ++j)
+			for(int j = 0; j < numCols; ++j)
 			{
-				isUnique(int[i][j] twoDArr)	
+				isUnique(twoDArr[i][j]);
 			}
 		}
-		for(int i = 0; i < uniqueVal.length(); ++i)
+		for(int i = 0; i < uniqueVal.length; ++i)
 		{
 			if(uniqueVal[i][1] == 0)	//if the value count is 0, we have reached the end of the list
 				break;
 			else
 			{
-				System.out.println("value: " + uniqueVal[i][0]);
+				details.append("\n").append("value: ").append(uniqueVal[i][0]);
+				details.append(" count: ").append(uniqueVal[i][1]);
 			}
 		}
-		//function to tell if a value is unique, basically searches array
-		bool isUnique(int value)
+		return details.toString();
+		//return "Not implemented";
+	}
+	
+	public boolean isUnique(int value)
+	//function to tell if a value is unique, basically searches array
+	{
+		//checks to see if the value in question is unique or not
+		for(int k = 0; k < uniqueVal.length; ++k)
 		{
-			//checks to see if the value in question is unique or not
-			for(int k = 0; k < uniqueVal.length(); ++k)
+			if(uniqueVal[k][0] == value)	//if the value already exists
 			{
-				if(uniqueVal[k][0] == value)	//if the value already exists
-				{
-					++uniqueVal[k][1];			//increment its count
-					return true;
-				}
-			}
-			//looks for open space in uniqueVal array to record a new unique value
-			for(int k = 0; k < uniqueVal.length(); ++k)
-			{
-				if(uniqueVal[k][1] == 0)
-				{
-					uniqueVal[k][0] = value;
-					uniqueVal[k][1] = 1;
-					return false;
-				}
+				++uniqueVal[k][1];			//increment its count
+				return true;
 			}
 		}
-		return "Not implemented";
-	}		
+		//looks for open space in uniqueVal array to record a new unique value
+		for(int k = 0; k < uniqueVal.length; ++k)
+		{
+			if(uniqueVal[k][1] == 0)
+			{
+				uniqueVal[k][0] = value;
+				uniqueVal[k][1] = 1;
+				return false;
+			}
+		}
+		return false;
+	}
 
 }
